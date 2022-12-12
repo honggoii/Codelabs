223d531c26c1accdd29e5cbdc460dbd5f3b03c4e
honggoii <honggoii@naver.com>
2022-12-13 01:31:04 +0900

    [추가] TextView에 데이터 개수 출력하기

--- RecyclerView/app/src/main/java/com/example/affirmations/MainActivity.kt
+++ RecyclerView/app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.affirmations
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.example.affirmations.data.Datasource
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView: TextView = findViewById(R.id.textview)
+        textView.text = Datasource().loadAffirmations().size.toString()
     }
 }
