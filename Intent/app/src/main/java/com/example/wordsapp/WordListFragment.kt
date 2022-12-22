277725812954d3f1bcb24526b577e7f55490faa9
honggoii <honggoii@naver.com>
2022-12-23 02:41:20 +0900

    [수정] DetailActivity -> Fragment 전환

--- Intent/app/src/main/java/com/example/wordsapp/WordListFragment.kt
+++ Intent/app/src/main/java/com/example/wordsapp/WordListFragment.kt
@@ -5,6 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.wordsapp.WordListFragment.Companion.LETTER
+import com.example.wordsapp.databinding.FragmentWordListBinding
+import kotlinx.coroutines.NonDisposableHandle.parent
 
 /**
  * A simple [Fragment] subclass.
@@ -13,4 +18,41 @@ import android.view.ViewGroup
  */
 class WordListFragment : Fragment() {
 
+    companion object {
+        const val LETTER = "letter"
+        const val SEARCH_PREFIX = "https://www.google.com/search?q="
+    }
+
+    private var _binding:FragmentWordListBinding? = null
+    private val binding
+        get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentWordListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        val letterId = activity?.intent?.extras?.getString(WordListFragment.LETTER).toString()
+
+        val recyclerView = binding.recyclerView
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = context?.let { WordAdapter(letterId, it) }
+
+        // Adds a [DividerItemDecoration] between items
+        recyclerView.addItemDecoration(
+            DividerItemDecoration(context, DividerItemDecoration.VERTICAL)
+        )
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
