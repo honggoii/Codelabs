f3e1c9eeee26b1c0c735cd2a50c844d26f7403be
honggoii <honggoii@naver.com>
2022-12-29 01:44:12 +0900

    [수정] 가격 $ 포맷 형식 적용

--- SharedViewModel/app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
+++ SharedViewModel/app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
@@ -2,7 +2,9 @@ package com.example.cupcake.model
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
+import java.text.NumberFormat
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -12,17 +14,19 @@ private const val PRICE_FOR_DAME_DAY_PICKUP = 3.00
 class OrderViewModel : ViewModel() {
 
     private val _quantity = MutableLiveData<Int>()
-
     val quantity: LiveData<Int> = _quantity
+
     private val _flavor = MutableLiveData<String>()
-
     val flavor: LiveData<String> = _flavor
+
     private val _date = MutableLiveData<String>()
-
     val date: LiveData<String> = _date
-    private val _price = MutableLiveData<Double>()
 
-    val price: LiveData<Double> = _price
+    private val _price = MutableLiveData<Double>()
+    val price: LiveData<String> = Transformations.map(_price) {
+        NumberFormat.getCurrencyInstance().format(it)
+    }
+
     val dateOptions = getPickupOptions()
 
     init {
