31c5ac844c4ad2336caf8d0f8180e28c4e74ad31
honggoii <honggoii@naver.com>
2023-01-02 23:19:56 +0900

    [추가] sliding pane listener

--- SlidingPaneLayout/app/src/main/java/com/example/android/sports/SportsListFragment.kt
+++ SlidingPaneLayout/app/src/main/java/com/example/android/sports/SportsListFragment.kt
@@ -61,8 +61,26 @@ class SportsListFragment : Fragment() {
 
 class SportsListOnBackPressedCallback(
     private val slidingPaneLayout: SlidingPaneLayout
-) : OnBackPressedCallback(slidingPaneLayout.isSlideable && slidingPaneLayout.isOpen) {
+) : OnBackPressedCallback(slidingPaneLayout.isSlideable && slidingPaneLayout.isOpen),
+SlidingPaneLayout.PanelSlideListener {
+
+    init {
+        slidingPaneLayout.addPanelSlideListener(this)
+    }
+
     override fun handleOnBackPressed() {
         slidingPaneLayout.closePane()
+    }
+
+    override fun onPanelSlide(panel: View, slideOffset: Float) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onPanelOpened(panel: View) {
+        isEnabled = true
+    }
+
+    override fun onPanelClosed(panel: View) {
+        isEnabled = false
     }
 }
