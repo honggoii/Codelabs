6ea7fe9d9455d8782d1fff5bed7caf013495dbdb
honggoii <honggoii@naver.com>
2023-01-05 21:54:42 +0900

    [추가] 웹 서버 사진 가져오는 interface 생성

--- Network/app/src/main/java/com/example/android/marsphotos/network/MarsApiService.kt
+++ Network/app/src/main/java/com/example/android/marsphotos/network/MarsApiService.kt
@@ -2,6 +2,7 @@ package com.example.android.marsphotos.network
 
 import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
 
 private const val BASE_URL =
     "https://android-kotlin-fun-mars-server.appspot.com"
@@ -9,3 +10,8 @@ private val retrofit = Retrofit.Builder()
     .addConverterFactory(ScalarsConverterFactory.create())
     .baseUrl(BASE_URL)
     .build()
+
+interface MarsApiService {
+    @GET("photos")
+    fun getPhotos(): String
+}
