fbb4391eed26981c2425bbceefbf53f9a5a4bd19
honggoii <honggoii@naver.com>
2023-01-14 00:35:39 +0900

    [수정] Flow 적용

--- Room/app/src/main/java/com/example/busschedule/StopScheduleFragment.kt
+++ Room/app/src/main/java/com/example/busschedule/StopScheduleFragment.kt
@@ -21,6 +21,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.coroutineScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.busschedule.databinding.StopScheduleFragmentBinding
@@ -29,6 +30,7 @@ import com.example.busschedule.viewmodels.BusScheduleViewModelFactory
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
 class StopScheduleFragment: Fragment() {
@@ -77,8 +79,10 @@ class StopScheduleFragment: Fragment() {
 
         val busStopAdapter = BusStopAdapter({})
         recyclerView.adapter = busStopAdapter
-        GlobalScope.launch(Dispatchers.IO) {
-            busStopAdapter.submitList(viewModel.scheduleForStopName(stopName))
+        lifecycle.coroutineScope.launch {
+            viewModel.scheduleForStopName(stopName).collect() {
+                busStopAdapter.submitList(it)
+            }
         }
     }
 
