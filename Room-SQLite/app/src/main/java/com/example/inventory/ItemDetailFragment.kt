78f1c99768e0075756e9ce5c94c8579e842a69ba
honggoii <honggoii@naver.com>
2023-01-16 06:49:49 +0900

    [추가] 아이템 삭제 함수 구현

--- Room-SQLite/app/src/main/java/com/example/inventory/ItemDetailFragment.kt
+++ Room-SQLite/app/src/main/java/com/example/inventory/ItemDetailFragment.kt
@@ -63,6 +63,7 @@ class ItemDetailFragment : Fragment() {
             itemCount.text = item.quantityInStock.toString()
             sellItem.isEnabled = viewModel.isStockAvailable(item)
             sellItem.setOnClickListener { viewModel.sellItem(item) }
+            deleteItem.setOnClickListener { showConfirmationDialog() }
         }
     }
 
@@ -94,6 +95,7 @@ class ItemDetailFragment : Fragment() {
      * Deletes the current item and navigates to the list fragment.
      */
     private fun deleteItem() {
+        viewModel.deleteItem(item)
         findNavController().navigateUp()
     }
 
