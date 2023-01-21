fd2a22bb44c67e3294498b8d3401e2fc97ffd08f
honggoii <honggoii@naver.com>
2023-01-22 01:30:42 +0900

    [추가] WorkInfo 태그

--- WorkManager/app/src/main/java/com/example/background/BlurViewModel.kt
+++ WorkManager/app/src/main/java/com/example/background/BlurViewModel.kt
@@ -20,12 +20,14 @@ import android.app.Application
 import android.content.ContentResolver
 import android.content.Context
 import android.net.Uri
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.work.Data
 import androidx.work.ExistingWorkPolicy
 import androidx.work.OneTimeWorkRequest
 import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkInfo
 import androidx.work.WorkManager
 import com.example.background.workers.BlurWorker
 import com.example.background.workers.CleanupWorker
@@ -38,9 +40,11 @@ class BlurViewModel(application: Application) : ViewModel() {
 
     internal var imageUri: Uri? = null
     internal var outputUri: Uri? = null
+    internal val outputWorkInfos: LiveData<List<WorkInfo>>
 
     init {
         imageUri = getImageUri(application.applicationContext)
+        outputWorkInfos = workManager.getWorkInfosByTagLiveData(TAG_OUTPUT)
     }
     /**
      * Create the WorkRequest to apply the blur and save the resulting image
@@ -67,7 +71,9 @@ class BlurViewModel(application: Application) : ViewModel() {
         }
 
         // Add "Save image to the File" WorkRequest
-        val save = OneTimeWorkRequest.Builder(SaveImageToFileWorker::class.java).build()
+        val save = OneTimeWorkRequestBuilder<SaveImageToFileWorker>()
+            .addTag(TAG_OUTPUT)
+            .build()
 
         // Chaining
         continuation = continuation.then(save)
