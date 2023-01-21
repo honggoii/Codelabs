aa94476a4ba2434cd026bc43a8074c3dc103d804
honggoii <honggoii@naver.com>
2023-01-21 19:13:20 +0900

    [추가] DataStore에서 값 읽는 경우 에러 처리

--- PreferencesDataStore/app/src/main/java/com/example/wordsapp/data/SettingDataStore.kt
+++ PreferencesDataStore/app/src/main/java/com/example/wordsapp/data/SettingDataStore.kt
@@ -5,9 +5,12 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
 import androidx.datastore.preferences.preferencesDataStore
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
+import java.io.IOException
 
 private const val LAYOUT_PREFERENCES_NAME = "layout_preferences"
 
@@ -19,6 +22,14 @@ class SettingDataStore(context: Context) {
     private val IS_LINEAR_LAYOUT_MANAGER = booleanPreferencesKey("is_linear_layout_manager")
 
     val preferences: Flow<Boolean> = context.dataStore.data
+        .catch {
+            if (it is IOException) {
+                it.printStackTrace()
+                emit(emptyPreferences())
+            } else {
+                throw it
+            }
+        }
         .map { preferences ->
             preferences[IS_LINEAR_LAYOUT_MANAGER] ?: true
         }
