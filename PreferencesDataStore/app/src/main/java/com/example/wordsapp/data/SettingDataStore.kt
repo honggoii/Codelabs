d60ca9f4d7813baac3a4e4546fac12f6d685e2b8
honggoii <honggoii@naver.com>
2023-01-21 19:07:06 +0900

    [추가] DataStore에 저장할 key

--- PreferencesDataStore/app/src/main/java/com/example/wordsapp/data/SettingDataStore.kt
+++ PreferencesDataStore/app/src/main/java/com/example/wordsapp/data/SettingDataStore.kt
@@ -2,6 +2,7 @@ package com.example.wordsapp.data
 
 import android.content.Context
 import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import java.util.prefs.Preferences
 
@@ -13,3 +14,5 @@ private const val LAYOUT_PREFERENCES_NAME = "layout_preferences"
 private val Context.dataStore : DataStore<Preferences> by preferencesDataStore(
     name = LAYOUT_PREFERENCES_NAME
 )
+
+private val IS_LINEAR_LAYOUT_MANAGER = booleanPreferencesKey("is_linear_layout_manager")
