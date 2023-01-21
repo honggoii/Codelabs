0ed4dbcb51569cfa625ec3f7f76434ebf3dc00cd
honggoii <honggoii@naver.com>
2023-01-21 19:10:57 +0900

    [추가] DataStore에 현재 설정된 레이아웃 저장

--- PreferencesDataStore/app/src/main/java/com/example/wordsapp/data/SettingDataStore.kt
+++ PreferencesDataStore/app/src/main/java/com/example/wordsapp/data/SettingDataStore.kt
@@ -4,14 +4,21 @@ import android.content.Context
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
+import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.preferencesDataStore
 
 private const val LAYOUT_PREFERENCES_NAME = "layout_preferences"
 
-private val Context.dataStore : DataStore<Preferences> by preferencesDataStore(
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(
     name = LAYOUT_PREFERENCES_NAME
 )
 
 class SettingDataStore(context: Context) {
     private val IS_LINEAR_LAYOUT_MANAGER = booleanPreferencesKey("is_linear_layout_manager")
+
+    suspend fun saveLayoutToPreferencesStore(isLinearLayoutManager: Boolean, context: Context) {
+        context.dataStore.edit { preferences ->
+            preferences[IS_LINEAR_LAYOUT_MANAGER] = isLinearLayoutManager
+        }
+    }
 }
