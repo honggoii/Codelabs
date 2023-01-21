365c7786a876c177951d25fc1737df5cb32da84f
honggoii <honggoii@naver.com>
2023-01-21 19:09:06 +0900

    [수정] key 클래스 안으로 이동

--- PreferencesDataStore/app/src/main/java/com/example/wordsapp/data/SettingDataStore.kt
+++ PreferencesDataStore/app/src/main/java/com/example/wordsapp/data/SettingDataStore.kt
@@ -6,13 +6,12 @@ import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
 import java.util.prefs.Preferences
 
-class SettingDataStore(context: Context) {
-}
-
 private const val LAYOUT_PREFERENCES_NAME = "layout_preferences"
 
 private val Context.dataStore : DataStore<Preferences> by preferencesDataStore(
     name = LAYOUT_PREFERENCES_NAME
 )
 
-private val IS_LINEAR_LAYOUT_MANAGER = booleanPreferencesKey("is_linear_layout_manager")
+class SettingDataStore(context: Context) {
+    private val IS_LINEAR_LAYOUT_MANAGER = booleanPreferencesKey("is_linear_layout_manager")
+}
